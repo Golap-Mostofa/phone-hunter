b3095e4231a9c2fafb7ed505337070b22f7569da
Golap-mostofa <mostofa6410@gmail.com>
2022-03-01 21:33:25 +0600

    card

--- app.js
+++ app.js
@@ -3,11 +3,17 @@
     const inputFilt = document.getElementById('input-field')
     const inputvalu = inputFilt.value;
     inputFilt.value = '';
+
+    const error = document.getElementById('error')
     if(inputvalu == ''){
-        console.log('rong input');
+        
+        error.style.display = 'block'
+        error.innerText = 'pleash valid input'
     }else{
+        error.style.display = 'none'
         const thamnail = document.getElementById('thamnail')
         thamnail.style.display = 'none'
+
         const url = `https://openapi.programming-hero.com/api/phones?search=${inputvalu}`
         fetch(url)
         .then(res=>res.json())
@@ -19,27 +25,39 @@
  const hendelClick = (prodacts)=>{
      const container = document.getElementById('container')
      container.textContent = '';
-    prodacts.forEach(prodact => {
-        // console.log(prodact);
-        const div = document.createElement('div')
-        div.classList.add('col-lg-4')
-        div.classList.add('col-sm-12')
-       
-        
-        div.innerHTML= `
-        <div class=" ">
-             <img  src="${prodact.image}" class="img-fluid" alt="...">
-             <div class="card-body">
-               <h5 class="card-title text-primary">${prodact.phone_name}</h5>
-               <p class="card-text text-white">${prodact.slug}</p>
 
-               <button class="btn btn-success" onclick="prodactInfo('${prodact.slug}')">About device</button>
-         </div>      
+     const error = document.getElementById('error')
+     if(prodacts.length == 0){
+        error.style.display = 'block'
+        error.innerText = 'No Result found'
+        const thamnail = document.getElementById('thamnail')
+        thamnail.style.display = 'block'
+     }else{
+        prodacts.forEach(prodact => {
+            
+            // console.log(prodact);
+            const div = document.createElement('div')
+            div.classList.add('col-lg-4')
+            div.classList.add('col-sm-12')
            
-        `;
-       
-        container.appendChild(div)
-    });
+            
+            div.innerHTML= `
+            <div class=" ">
+                 <img  src="${prodact.image}" class="img-fluid" alt="...">
+                 <div class="card-body">
+                   <h5 class="card-title text-primary">${prodact.phone_name}</h5>
+                   <p class="card-text text-white">${prodact.slug}</p>
+    
+                   <button class="btn btn-success" onclick="prodactInfo('${prodact.slug}')">About device</button>
+             </div>      
+               
+            `;
+           
+            container.appendChild(div)
+        
+        });
+     }
+    
  }
 
 
@@ -76,6 +94,6 @@
             </div> 
                 `
         detelsContainer.appendChild(creatDiv)
-         
+         console.log(detels);
  }
  
