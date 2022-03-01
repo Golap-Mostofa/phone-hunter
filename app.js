6c38be8e581e60c0767bd7641c532c2f4006df0e
Golap-mostofa <mostofa6410@gmail.com>
2022-03-01 10:26:19 +0600

    card

--- app.js
+++ app.js
@@ -2,8 +2,12 @@
  const buttonHendelar = ()=>{
     const inputFilt = document.getElementById('input-field')
     const inputvalu = inputFilt.value;
+    inputFilt.value = '';
     if(inputvalu == ''){
         console.log('rong input');
+    }else if(inputFilt.length === 0){
+        console.log('pleash carent value');
+
     }else{
         const url = `https://openapi.programming-hero.com/api/phones?search=${inputvalu}`
         fetch(url)
@@ -15,6 +19,7 @@
 
  const hendelClick = (prodacts)=>{
      const container = document.getElementById('container')
+     container.textContent = '';
     prodacts.forEach(prodact => {
         console.log(prodact);
         const div = document.createElement('div')
@@ -29,11 +34,16 @@
                <h5 class="card-title text-white">${prodact.phone_name}</h5>
                <p class="card-text text-white">${prodact.slug}</p>
 
-               <button class="btn btn-success" onclick="prodactInfo(${prodact})">About device</button>
+               <button class="btn btn-success" onclick="prodactInfo(${prodact = prodact.slug})">About device</button>
          </div>      
            
         `;
        
         container.appendChild(div)
     });
+ }
+
+
+ const prodactInfo = (prodact)=>{
+    console.log(prodact);
  }
